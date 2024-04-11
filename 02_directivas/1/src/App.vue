b21ccecd1bb9ddb00785ce4a02c40fc7e0ce2b0c
Jon K. Gomez <jonkandidi.gomez@ikasle.egibide.org>
2024-04-11 21:57:14 +0200

    ejercicio1

---
+++ 02_directivas/1/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+const datos = ref({
+    nombre: '',
+    apellido: ''
+});
+</script>
+
+<template>
+    <div class="container pt-5">
+        <h1>Datos Personales</h1>
+        <div class="card col-4 p-3">
+            <div class="mb-3">
+                <label class="form-label" for="nombre">Nombre</label>
+                <input class="form-control" id="nombre" type="text" v-model="datos.nombre"/>
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="apellido">Apellido</label>
+                <input class="form-control" id="apellido" type="text" v-model="datos.apellido"/>
+            </div>
+            <div class="card-text ">
+                <p>Tu nombre completo es: {{datos.nombre}} {{datos.apellido}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
